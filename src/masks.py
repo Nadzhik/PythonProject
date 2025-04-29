0b7aaac83c6fb99302ef79865e70ba856e6d3434
Nadzhi <klopec31@gmail.com>
2025-04-29 03:00:46 +0300

    fixies masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(user_card_number: str) -> str:
     """Функция принимает на вход номер карты в виде числа и возвращает маску номера"""
-    if not user_card_number.isdigit():
+    if not user_card_number.isdigit() or len(user_card_number) != 16:
         raise ValueError("Номер карты должен содержать числа.")
     masked_number = f"{user_card_number[:4]} {user_card_number[4:6]}** **** {user_card_number[12:]}"
 
@@ -9,7 +9,7 @@ def get_mask_card_number(user_card_number: str) -> str:
 
 def get_mask_account(check_number: str) -> str:
     """Функция принимает на вход номер счета в виде числа и возвращает маску номера"""
-    if not check_number.isdigit():
+    if not check_number.isdigit() or len(check_number) < 4:
         raise ValueError("Номер карты должен содержать числа.")
     masked_check = f"**{check_number[-4:]}"
 
